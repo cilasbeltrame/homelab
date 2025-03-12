c848b6922f5a7aeb2c5d8785ccec593eba0802b6
Cilas Beltrame <cilasbeltrame@hotmail.com>
2025-03-12 11:42:04 -0300

    feat(): configured ssh setup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,5 +13,24 @@ if [ ! -f ".runner" ]; then
   ./config.sh --url "$GH_REPO_URL" --token "$GH_RUNNER_TOKEN" --unattended
 fi
 
+# Configure SSH pub key from docker host
+if [[ ! -z "$GH_RUNNER_SSH_KEY" && ! -z "$DOCKER_HOST_SSH" ]]; then
+  mkdir -p /runner/.ssh
+  echo "$GH_RUNNER_SSH_KEY" > /runner/.ssh/id_rsa
+  chmod 600 /runner/.ssh/id_rsa
+  # Start SSH agent in the background
+  eval "$(ssh-agent -s)"
+  # Add SSH key to agent
+  ssh-add /runner/.ssh/id_rsa
+
+  echo "Host docker-host
+  HostName $DOCKER_HOST_SSH
+  User ${DOCKER_HOST_USER:-root}
+  IdentityFile /runner/.ssh/id_rsa
+  StrictHostKeyChecking no
+  PasswordAuthentication no" > /runner/.ssh/config
+  chmod 600 /runner/.ssh/config
+fi
+
 # Start the runner
 exec ./run.sh "$@" 
